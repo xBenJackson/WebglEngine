ed33b5381e162660893aa351bda9b1b06adab988
xBenJackson <131803252+xBenJackson@users.noreply.github.com>
2023-04-25 23:19:58 -0400

    Update WebglEngine.js

--- WebglEngine.js
+++ WebglEngine.js
@@ -1,1 +1,21 @@
+/*
+    Virtual Engine
+    Webgl based, 3-Dimensional engine
 
+    Project start: 4-24-23
+    Version: 1
+
+    Made by: Ben Jackson
+*/
+
+/* .. Import Dependencies .. */
+import Vector2 from './engine/maths/Vector2.js';
+
+/* .. Create Engine .. */
+const VE = {};
+
+/* .. Include Dependencies .. */
+VE.Vector2 = Vector2;
+
+/* .. Export Engine .. */
+export default VE;
