85a6b63c33617ff23502fdaa4e391961d8c18731
xBenJackson <131803252+xBenJackson@users.noreply.github.com>
2023-04-28 00:44:35 -0400

    Update WebglEngine.js

--- WebglEngine.js
+++ WebglEngine.js
@@ -12,6 +12,7 @@ import Scene from './source/core/Scene.js';
 import Color from './source/maths/Color.js';
 import Vector2 from './source/maths/Vector2.js';
 import Vector3 from './source/maths/Vector3.js';
+import Matrix from './source/maths/Matrix.js';
 
 /* .. Create Engine .. */
 const Engine = {};
@@ -22,6 +23,7 @@ Engine.Scene = Scene;
 Engine.Color = Color;
 Engine.Vector2 = Vector2;
 Engine.Vector3 = Vector3;
+Engine.Matrix = Matrix;
 
 /* .. Export Engine .. */
 export default Engine;
